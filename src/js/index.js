e7f7cb0f8058be5dc2fe1de3ae94423c52d07744
MireyaCR <mireya.mcr@gmail.com>
2022-10-13 18:28:40 +0000

    simplecounter

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,12 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+// import Home from "./component/home.jsx";
+
+function SimpleCounter(props){
+    return (<p>Hello World</p>)
+}
+
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
